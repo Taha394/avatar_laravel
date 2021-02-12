f8c3253a8bbc4a1345f6771e973374d7ea40cf4d
Taha394 <Salahtaha741@gmail.com>
2021-02-12 03:12:28 +0200

    form in curd with eloquent

---
+++ resources/views/offers/create.blade.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 50px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-top: 100px;
+            }
+        </style>
+    </head>
+    <body>
+            <div class="content">
+                <div class="title m-b-md">
+                    Add Your offer
+                </div>
+            </div>
+            @if(Session::has('success'))
+            <div class="alert alert-success" role="alert">
+                {{Session::get('success')}}
+            </div>
+            @endif
+            <form method="post" action="{{route('offers.store')}}">
+                @csrf
+                <div class="form-group">
+                    <label for="exampleInputEmail1">offer name</label>
+                    <input type="text" class="form-control" name="name" placeholder="offer name">
+                    @error('name')
+                    <small  class="form-text text-danger">{{$message}}</small>
+                    @enderror
+
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">offer price</label>
+                    <input type="number" class="form-control" name="price" placeholder="offer price">
+                    @error('price')
+                    <small  class="form-text text-danger">{{$message}}</small>
+                    @enderror
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">offer details</label>
+                    <input type="text" class="form-control" name="details" placeholder="offer details">
+                    @error('details')
+                    <small  class="form-text text-danger">{{$message}}</small>
+                    @enderror
+
+                </div>
+                <button type="submit" class="btn btn-primary">save offer</button>
+            </form>
+    </body>
+</html>
